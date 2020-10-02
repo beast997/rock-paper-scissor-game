0222ab1bbc1ba6fdd8619a75917d8fa0dd9639e0
Rahul Gupta <68496473+beast997@users.noreply.github.com>
2020-10-02 08:27:12 +0500

    Update the game
    added some more features like printing the instruction to user and used the for loop for iteration

--- Rock Paper Scissor Game.py
+++ Rock Paper Scissor Game.py
@@ -3,31 +3,34 @@ import random
 user = None
 
 computer = random.choice(['rock','paper','scissor'])
-
+notification = print('please enter the value you wish to choose or press q to quit the game:')
 while user == None:
-    user = input('please enter the value you wish to choose:')
+    user = input('please enter the value you wish to choose or press q to quit the game:')
     if user not in ['rock','paper','scissor']:
         print('You need to choose valid value')
         user = None
-
-
-if user == computer:
-    print('Draw')
-if user == 'rock':
-    if computer == 'paper':
-        print('computer wins')
-    elif computer == 'scissor':
-        print('user wins')
-if user == 'paper':
-    if computer == 'scissor':
-        print('computer wins')
-    elif computer == 'rock':
-        print('user wins')
-if user == 'scissor':
-    if computer == 'rock':
-        print('computer wins')
-    elif computer == 'paper':
-        print('user wins')
+i = 1
+for(i<=3):           //Iterate The Loop Three Times
+    user = input('please enter the value you wish to choose:')
+    if user == computer:
+        print('Draw')
+    if user == 'rock':
+        if computer == 'paper':
+            print('computer wins')
+        elif computer == 'scissor':
+            print('user wins')
+    if user == 'paper':
+        if computer == 'scissor':
+            print('computer wins')
+        elif computer == 'rock':
+            print('user wins')
+    if user == 'scissor':
+        if computer == 'rock':
+            print('computer wins')
+        elif computer == 'paper':
+            print('user wins')
+    elif user=='q':
+        exit()
 
 print('computer is {}'.format(computer))
 print('user is {}'.format(user))
